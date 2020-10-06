efad0cb9b8112695a7bfade75c540245c305c1bd
17639312923 <2095886084@.com>
2020-10-06 18:19:45 +0800

    验证规则

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,10 +3,24 @@ $(function(){
     $('#link-reg').on('click',function(){
         $('.login-box').hide()
         $('.reg-box').show()
-    })
+    });
     // 点击登录
     $('#link-login').on('click',function(){
         $('.login-box').show()
         $('.reg-box').hide()
+    });
+    // 自定义验证
+    var form = layui.form;
+    form.verify({
+        pass: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+          ],
+        repass:function(value){
+            let pwd = $('.reg-box [name=password]').val();
+            if(pwd !== value){
+                return '两次密码不一致'
+            }
+        }
     })
 })
