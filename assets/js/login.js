d11bf820cfbcc7b33efba33834e3ae1afed12bc4
17639312923 <2095886084@.com>
2020-10-07 10:53:53 +0800

    完成登录和注册

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,6 +11,7 @@ $(function(){
     });
     // 自定义验证
     var form = layui.form;
+    var layer = layui.layer;
     form.verify({
         pass: [
             /^[\S]{6,12}$/
@@ -22,5 +23,45 @@ $(function(){
                 return '两次密码不一致'
             }
         }
+    });
+    // 根路径
+    var root ='http://ajax.frontend.itheima.net'
+    // 用户名注册
+    
+    $('#form_reg').on('submit',function(e){
+        // 阻止表单默认提交事件
+        e.preventDefault();
+        var data = {username:$('#form_reg [name=username]').val(),password:$('#form_reg [name=password]').val()};
+        $.ajax({
+            type: "POST",
+            url: root+"/api/reguser",
+            data: data,
+            
+            success: function (res) {
+                // console.log(res);
+                if(res.status!==0)return layer.msg(res.message);
+                layer.msg(res.message);
+                $('#link-login').click()
+            }
+        });
+    });
+    // 用户登录
+    $('#form_login').on('submit',function(e){
+        // 阻止默认提交
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: root+"/api/login",
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if(res.status!==0)return layer.msg(res.message);
+                layer.msg(res.message);
+                // 保存token
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        });
     })
 })
